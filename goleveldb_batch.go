de5a944252177b9c923df17d1b6a12ccec1a00bf
Erik Grinaker <erik@interchain.berlin>
2020-03-10 10:28:59 +0100

    Ported GoLevelDB to new batch interface

--- goleveldb_batch.go
+++ goleveldb_batch.go
@@ -12,38 +12,60 @@ type goLevelDBBatch struct {
 
 var _ Batch = (*goLevelDBBatch)(nil)
 
+func newGoLevelDBBatch(db *GoLevelDB) *goLevelDBBatch {
+	return &goLevelDBBatch{
+		db:    db,
+		batch: new(leveldb.Batch),
+	}
+}
+
 // Set implements Batch.
 func (b *goLevelDBBatch) Set(key, value []byte) error {
+	if b.batch == nil {
+		return errBatchClosed
+	}
 	b.batch.Put(key, value)
 	return nil
 }
 
 // Delete implements Batch.
 func (b *goLevelDBBatch) Delete(key []byte) error {
+	if b.batch == nil {
+		return errBatchClosed
+	}
 	b.batch.Delete(key)
 	return nil
 }
 
 // Write implements Batch.
 func (b *goLevelDBBatch) Write() error {
+	if b.batch == nil {
+		return errBatchClosed
+	}
 	err := b.db.db.Write(b.batch, &opt.WriteOptions{Sync: false})
 	if err != nil {
 		return err
 	}
-	return nil
+	return b.Close()
 }
 
 // WriteSync implements Batch.
 func (b *goLevelDBBatch) WriteSync() error {
+	if b.batch == nil {
+		return errBatchClosed
+	}
 	err := b.db.db.Write(b.batch, &opt.WriteOptions{Sync: true})
 	if err != nil {
 		return err
 	}
-	return nil
+	return b.Close()
 }
 
 // Close implements Batch.
 func (b *goLevelDBBatch) Close() error {
-	b.batch.Reset()
+	if b.batch != nil {
+		b.batch.Reset()
+		b.batch = nil
+	}
 	return nil
 }
