e7c033fabc6512394c67e3158b8335026f2adb5e
Erik Grinaker <erik@interchain.berlin>
2020-03-10 18:05:08 +0100

    Documented that MemDB iterators take out read locks

--- memdb.go
+++ memdb.go
@@ -154,11 +154,13 @@ func (db *MemDB) NewBatch() Batch {
 }
 
 // Iterator implements DB.
+// Takes out a read-lock on the database until the iterator is closed.
 func (db *MemDB) Iterator(start, end []byte) (Iterator, error) {
 	return newMemDBIterator(db, start, end, false), nil
 }
 
 // ReverseIterator implements DB.
+// Takes out a read-lock on the database until the iterator is closed.
 func (db *MemDB) ReverseIterator(start, end []byte) (Iterator, error) {
 	return newMemDBIterator(db, start, end, true), nil
 }
