3c6d50c5bf3962e940f72fe81615cadf4859f1ca
romelukaku <gundamaster5@gmail.com>
2022-04-12 16:17:01 +0700

    fix pebble iterator

--- pebble.go
+++ pebble.go
@@ -195,7 +195,7 @@ func (db *PebbleDB) Iterator(start, end []byte) (Iterator, error) {
 		LowerBound: start,
 		UpperBound: end,
 	}
-	itr := db.DB().NewIter(&o)
+	itr := db.db.NewIter(&o)
 	itr.First()
 
 	return newPebbleDBIterator(itr, start, end, false), nil
@@ -211,5 +211,6 @@ func (db *PebbleDB) ReverseIterator(start, end []byte) (Iterator, error) {
 		UpperBound: end,
 	}
 	itr := db.db.NewIter(&o)
+	itr.Last()
 	return newPebbleDBIterator(itr, start, end, true), nil
 }
