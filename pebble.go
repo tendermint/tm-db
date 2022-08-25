bac3b3561938d4ce915209cf96be2325e01c6dd3
Marko Baricevic <marbar3778@yahoo.com>
2022-08-25 17:14:32 +0200

    add more compaction

--- pebble.go
+++ pebble.go
@@ -73,43 +73,9 @@ var _ DB = (*PebbleDB)(nil)
 
 func NewPebbleDB(name string, dir string) (DB, error) {
 	dbPath := filepath.Join(dir, name+".db")
-	//	cache := pebble.NewCache(1024 * 1024 * 32)
-	//	defer cache.Unref()
 	opts := &pebble.Options{
-		//		Cache:                       cache,
-		//		FormatMajorVersion:          pebble.FormatNewest,
-		//		L0CompactionThreshold:       2,
-		//		L0StopWritesThreshold:       1000,
-		//		LBaseMaxBytes:               64 << 20, // 64 MB
-		//		Levels:                      make([]pebble.LevelOptions, 7),
-		//		MaxConcurrentCompactions:    3,
-		//		MaxOpenFiles:                1024,
-		//		MemTableSize:                64 << 20,
-		//		MemTableStopWritesThreshold: 4,
+		MaxConcurrentCompactions: func() int { return 3 }, // default 1
 	}
-	/*
-		for i := 0; i < len(opts.Levels); i++ {
-			l := &opts.Levels[i]
-			l.BlockSize = 32 << 10       // 32 KB
-			l.IndexBlockSize = 256 << 10 // 256 KB
-			l.FilterPolicy = bloom.FilterPolicy(10)
-			l.FilterType = pebble.TableFilter
-			if i > 0 {
-				l.TargetFileSize = opts.Levels[i-1].TargetFileSize * 2
-			}
-			l.EnsureDefaults()
-		}
-	*/
-	//	opts.Levels[6].FilterPolicy = nil
-	//	opts.FlushSplitBytes = opts.Levels[0].TargetFileSize
-
-	//for i := 0; i < len(opts.Levels); i++ {
-	//	l := &opts.Levels[i]
-	//	l.Compression = pebble.NoCompression
-	//	l.EnsureDefaults()
-	//}
-	//
-	//opts.DisableAutomaticCompactions = true
 
 	opts.EnsureDefaults()
 
