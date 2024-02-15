b420fc4300c55e08be715694b87e5026ebf45ed1
Jacob Gadikian <jacobgadikian@gmail.com>
2024-02-15 20:35:04 +0530

    lint

--- pebble.go
+++ pebble.go
@@ -158,7 +158,7 @@ func (db *PebbleDB) Compact(start, end []byte) (err error) {
 		end = append(end, iter.Key()...)
 	}
 	err = db.db.Compact(start, end, true)
-	return
+	return err
 }
 
 // Close implements DB.
